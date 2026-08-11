7a0819235be1f5f26cb892f574a418e7d9a92f05
Hassan Ali <naeemhassan786000@gmail.com>
2026-08-11 17:03:38 +0500

    first

---
+++ updrade.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Welcome to the Git_Github Advance Journey\n";
+    string st = "Evolution";
+    string final = st + "Era";
+    cout << "Final: " << final << endl;
+    string st = "Evolution";
+    string final = st + "Era";
+    cout << "Final: " << final << endl;
+    char arr[5] = {'t', 'o', 'd', 'o', '!'};
+    cout << "Array: ";
+    for (int i = 0; i < sizeof(arr); i++)
+    {
+        cout << arr[i];
+    }
+}
