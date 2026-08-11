49002003f544087382d7901b690f6cc4775532b5
Hassan Ali <naeemhassan786000@gmail.com>
2026-08-11 17:30:24 +0500

    line++

--- goit.cpp
+++ goit.cpp
@@ -3,13 +3,6 @@ using namespace std;
 int main()
 {
     cout << "Git Advancement\n";
-<<<<<<< HEAD
-<<<<<<< HEAD
     cout << "Git ++\n";
-=======
-    cout << "Git ++\n";
->>>>>>> 11b417a (line/feature)
-=======
-    cout << "Git ++\n";
->>>>>>> 11b417a24b6f9cab7064bd69036ca4d8eebb6350
+    cout << "Git Up\n";
 }
